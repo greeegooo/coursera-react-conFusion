1d64331a0d926d8500c95af549a8f7e02d4e6f0c
gmichalopulos <gregomicha@gmail.com>
2021-04-30 07:50:20 -0300

    Redux thunk

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,7 +8,7 @@ import About from './AboutComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Contact from './ContactComponent';
-import { addComment } from '../redux/ActionCreators';
+import { addComment, fetchDishes } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
   return {
@@ -22,12 +22,19 @@ const mapStateToProps = state => {
 const mapDispatchToProps = (dispatch) => ({
 
   addComment: (dishId, rating, author, comment) => 
-    dispatch(addComment(dishId, rating, author, comment))
+    dispatch(addComment(dishId, rating, author, comment)),
+
+  fetchDishes: () => dispatch(fetchDishes())
+
 });
 
 class Main extends Component {
 
-  selectedDish = (dishId) => this.props.dishes.filter((dish) => dish.id === dishId)[0];
+  componentDidMount() {
+    this.props.fetchDishes();
+  }
+
+  selectedDish = (dishId) => this.props.dishes.dishes.filter((dish) => dish.id === dishId)[0];
   selectedDishComments = (dishId) => this.props.comments.filter((comment) => comment.dishId === dishId);
 
   render() {
@@ -41,13 +48,17 @@ class Main extends Component {
           dish={this.selectedDish(dishId)} 
           comments={this.selectedDishComments(dishId)} 
           addComment={this.props.addComment}
+          isLoading={this.props.dishes.isLoading}
+          errMess={this.props.dishes.errMess}
         />
       );
     };
     const HomePage = () => {
       return(
           <Home 
-              dish={this.props.dishes.filter((dish) => dish.featured)[0]}
+              dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+              dishesLoading={this.props.dishes.isLoading}
+              dishesErrMess={this.props.dishes.errMess}
               promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
               leader={this.props.leaders.filter((leader) => leader.featured)[0]}
           />
